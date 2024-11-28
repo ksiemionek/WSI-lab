5d27299f41de1e7613586c74fed8238cb42366fa
ksiemion <01187279@pw.edu.pl>
2024-11-28 20:48:10 +0100

    Added function for analyzing results

--- lab3/main.py
+++ lab3/main.py
@@ -1,6 +1,10 @@
 import random
+import time
 
-from numpy.ma.core import left_shift
+import numpy as np
+from matplotlib import pyplot as plt
+from sympy.concrete.guess import find_simple_recurrence
+from sympy.polys.polyoptions import Greedy
 
 random.seed(8)  # For final results set seed as your student's id modulo 42
 
@@ -117,6 +121,65 @@ def main():
           f"First agent: {first_agent.numbers}\n"
           f"Second agent: {second_agent.numbers}")
 
+def run_games(agent1, agent2, games=1000, vector_size=15):
+    results1 = []
+    results2 = []
+    times = []
+    for game in range(games):
+        vector = [random.randint(-10, 10) for _ in range(vector_size)]
+        agent1.numbers = []
+        agent2.numbers = []
+
+        if game % 2 == 0:
+            first_agent, second_agent = agent1, agent2
+        else:
+            first_agent, second_agent = agent2, agent1
+
+        start_time = time.time()
+        run_game(vector, first_agent, second_agent)
+        final_time = time.time() - start_time
+
+        times.append(final_time)
+        if game % 2 == 0:
+            results1.append(sum(first_agent.numbers))
+            results2.append(sum(second_agent.numbers))
+        else:
+            results1.append(sum(second_agent.numbers))
+            results2.append(sum(first_agent.numbers))
+
+    return np.mean(times), np.mean(results1), np.std(results1), results1, np.mean(results2), np.std(results2), results2
+
+
+def analyze_results(first_agent, second_agent, games=1000, vector_size=15):
+    first_agent_name = str(first_agent.__class__.__name__)
+    second_agent_name = str(second_agent.__class__.__name__)
+    for depth in [1, 2, 3, 15]:
+        if first_agent_name == "MinMaxAgent":
+            first_agent.max_depth = depth
+        if second_agent_name == "MinMaxAgent":
+            second_agent.max_depth = depth
+        avg_time, avg_score1, std_dev1, scores1, avg_score2, std_dev2, scores2 = run_games(first_agent, second_agent, games, vector_size)
+
+        # Results presented in the table
+        print(f"Depth: {depth}, {games} games, average time: {avg_time:.6f}s")
+        print("+-------------+---------------+---------+")
+        print("|    Agent    | Average score | Std dev |")
+        print("+-------------+---------------+---------+")
+        print(f"| {first_agent_name:>11} | {avg_score1:>13.2f} | {std_dev1:>7.2f} |")
+        print("+-------------+---------------+---------+")
+        print(f"| {second_agent_name:>11} | {avg_score2:>13.2f} | {std_dev2:>7.2f} |")
+        print("+-------------+---------------+---------+\n")
+
+        # Histogram for depth 2 and 15
+        if depth in [2, 15]:
+            plt.hist(scores1, bins=20, color="red", alpha=0.5, label=first_agent_name)
+            plt.hist(scores2, bins=20, color="blue", alpha=0.5, label=second_agent_name)
+            plt.title(f"Depth = {depth}, {games} games")
+            plt.xlabel("Scores")
+            plt.ylabel("Frequency")
+            plt.legend()
+            plt.show()
+
 
 if __name__ == "__main__":
-    main()
+    analyze_results(GreedyAgent(), MinMaxAgent())
